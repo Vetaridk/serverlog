0318098d2eead25a7bf2e58124470d8a7fdaea42
Vetari <57092829+Vetaridk@users.noreply.github.com>
2020-06-10 08:32:04 +0200

    Ændringer

--- server.lua
+++ server.lua
@@ -7,7 +7,12 @@
 ░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝
 ]]--
 
-local footer = "Lavet af Vetari#1995"
+AddEventHandler('onClientResourceStart', function (resourceName)
+    if(GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    sendToDiscord(3447003, cfg.discordnavn, cfg.discordbesked, "Lavet af Vetari#1995")
+  end)
 
 function sendToDiscord(color, name, message, footer)
     local embed = {
@@ -16,10 +21,10 @@ function sendToDiscord(color, name, message, footer)
               ["title"] = "**".. cfg.discordnavn .."**",
               ["description"] = cfg.discordbesked,
               ["footer"] = {
-                  ["text"] = footer,
+                  ["text"] = "Lavet af Vetari#1995",
               },
           }
       }
   
-    PerformHttpRequest('DISCORD_URL', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest('cfg.discordwebhook', function(err, text, headers) end, 'POST', json.encode({username = cfg.discordnavn, embeds = embed}), { ['Content-Type'] = 'application/json' })
   end
