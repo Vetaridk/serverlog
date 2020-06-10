0b6210d06e46aca885c6d2a027b7c066857575ef
Vetari <57092829+Vetaridk@users.noreply.github.com>
2020-06-10 08:00:54 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,10 +1,10 @@
--[[
+--[[
 ██╗░░░██╗███████╗████████╗░█████╗░██████╗░██╗
 ██║░░░██║██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
 ╚██╗░██╔╝█████╗░░░░░██║░░░███████║██████╔╝██║
 ░╚████╔╝░██╔══╝░░░░░██║░░░██╔══██║██╔══██╗██║
 ░░╚██╔╝░░███████╗░░░██║░░░██║░░██║██║░░██║██║
 ░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝
-]]
+]]--
 
 PerformHttpRequest(config.discordwebhook, function(err, text, headers) end, 'POST', json.encode({username = config.discordnavn, content = config.discordbesked}), { ['Content-Type'] = 'application/json' })
