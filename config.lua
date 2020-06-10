aa2882764439a99262e464737d85f87cc8250630
Vetari <57092829+Vetaridk@users.noreply.github.com>
2020-06-10 08:24:18 +0200

    Ændringer

--- config.lua
+++ config.lua
@@ -9,6 +9,6 @@
 
 cfg = {}
 
-cfg.discordwebhook = "" -- Indsæt dit discord webhook her
+cfg.discordwebhook = "https://discordapp.com/api/webhooks/720161327989129217/A22EUk0WllEhVrgRLX57vPX5xzPXJ0GhhNunKrViV3HYuHeYossN2UqlZSVq8oMbty8m" -- Indsæt dit discord webhook her
 cfg.discordnavn = "SERVERNAVN - Server Status" -- Det navn botten skal hedde på discord
-cfg.discordbesked = " **Server 1 er på vej op!** ```IP: IP:PORT``` @here " -- Beskeden den sender i din valgte discord kanal
+cfg.discordbesked = "test" -- Beskeden den sender i din valgte discord kanal
--- server.lua
+++ server.lua
@@ -7,4 +7,19 @@
 ░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝
 ]]--
 
-PerformHttpRequest(cfg.discordwebhook, function(err, text, headers) end, 'POST', json.encode({username = cfg.discordnavn, content = cfg.discordbesked}), { ['Content-Type'] = 'application/json' })
+local footer = "Lavet af Vetari#1995"
+
+function sendToDiscord(color, name, message, footer)
+    local embed = {
+          {
+              ["color"] = 3447003,
+              ["title"] = "**".. cfg.discordnavn .."**",
+              ["description"] = cfg.discordbesked,
+              ["footer"] = {
+                  ["text"] = footer,
+              },
+          }
+      }
+  
+    PerformHttpRequest('DISCORD_URL', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+  end
